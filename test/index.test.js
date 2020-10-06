659724f4c98c6fb560675f24457683c7209ce9c3
Tom Wilson <twilson63@gmail.com>
2020-10-06 10:28:16 -0400

    updated tape to address security audit

--- test/index.test.js
+++ test/index.test.js
@@ -20,7 +20,8 @@ test('express server should listen and close', async t => {
 
   const result = await fetch(server.url).then(r => r.json())
   t.ok(result.ok)
-  server.close(() => t.end())
+
+  server.close()
 })
 
 
